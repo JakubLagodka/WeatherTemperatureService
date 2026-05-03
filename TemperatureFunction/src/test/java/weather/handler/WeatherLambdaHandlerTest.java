c401f73c064fe47540aa42af6ea28e2561993d85
jakub <jakub.lagodka.pl@gmail.com>
2026-05-03 17:17:10 +0200

    added handler package

--- TemperatureFunction/src/test/java/weather/handler/WeatherLambdaHandlerTest.java
+++ TemperatureFunction/src/test/java/weather/handler/WeatherLambdaHandlerTest.java
@@ -1,4 +1,4 @@
-package weather;
+package weather.handler;
 
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
@@ -12,7 +12,7 @@ import static org.junit.Assert.assertNotNull;
 public class WeatherLambdaHandlerTest {
   @Test
   public void successfulResponse() {
-    WeatherLambdaHandler  app = new WeatherLambdaHandler ();
+    WeatherLambdaHandler app = new WeatherLambdaHandler ();
     APIGatewayProxyResponseEvent result = app.handleRequest(new APIGatewayProxyRequestEvent().withQueryStringParameters(Map.of("city","Lublin")), null);
     assertEquals(200, result.getStatusCode().intValue());
     assertEquals("application/json", result.getHeaders().get("Content-Type"));
